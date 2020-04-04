37a5e8d8a84a0c7ec8b6ec67e5436bc502a71bd8
yogjun <280536928@qq.com>
2020-04-04 13:54:37 +0800

    eureka调整

--- yog-registry/src/main/java/cn/yog/registry/RegistryApplication.java
+++ yog-registry/src/main/java/cn/yog/registry/RegistryApplication.java
@@ -1,7 +1,8 @@
-package com.jzez.registry;
+package cn.yog.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
